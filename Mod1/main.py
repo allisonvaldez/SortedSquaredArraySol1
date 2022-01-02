6338ae4428e2258b2da91e5a05e40098a947b757
Allison Valdez <ajsnipes@usfca.edu>
2022-01-01 16:19:30 -0800

    finished

--- Mod1/main.py
+++ Mod1/main.py
@@ -11,12 +11,20 @@ def sorted_square_array(array):
     """
     sorted_squares = [0 for i in array]
 
+    """
+    here they created a for loop that returns the traversal of an array from 
+    start to finish and peforms the square on each index
+    """
     for i in range(len(array)):
         initial_value = array[i]
         sorted_squares[i] = initial_value * initial_value
 
+    # they sort the new array of squared values
     sorted_squares.sort()
+
+    # need to include the return call for the code to show values
     return sorted_squares
 
 
+# need to call print to display the values in the terminal
 print(sorted_square_array([0, 1, 2, 3]))
